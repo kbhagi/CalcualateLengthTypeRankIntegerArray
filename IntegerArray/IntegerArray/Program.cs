2ce95203da4e292a376f8f16360c887f0b8adde3
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 11:29:50 +0530

    Add files via upload

---
+++ IntegerArray/IntegerArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Console.WriteLine("Type=" + array.GetType());
+            Console.WriteLine("Rank=" + array.Rank);
+            Console.WriteLine("Length=" + array.Length);
+            Console.ReadKey();
+
+        }
+    }
+}
